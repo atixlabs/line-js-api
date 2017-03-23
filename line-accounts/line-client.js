529a308b8b49d209f68fab4e0689d24464b92ee3
Agustin Rosas <arosas@atixlabs.com>
2017-03-23 18:18:54 -0300

    Login with Line
    Description
    Configure LINE oauth service, and integrated with accoutns package

---
+++ line-accounts/line-client.js
@@ -0,0 +1,55 @@
+Meteor.loginWithLine = function(options, callback) {
+  // support a callback without options
+  if (! callback && typeof options === "function") {
+    callback = options;
+    options = null;
+  }
+  var credentialRequestCompleteCallback = Accounts.oauth.credentialRequestCompleteHandler(callback);
+  Line.requestCredential(options, credentialRequestCompleteCallback);
+};
+
+
+/*
+  Request Line credentials for the user.
+
+  @param options (optional)
+  @param credentialRequestCompleteCallback (callback *function* to call on completion. Takes one argument: credentialToken
+    on success, or Error on error)
+ */
+Line.requestCredential = function (options, credentialRequestCompleteCallback) {
+  // support both (options, callback) and (callback).
+  if (!credentialRequestCompleteCallback && typeof options === 'function') {
+    credentialRequestCompleteCallback = options;
+    options = {};
+  } else if (!options) {
+    options = {};
+  }
+
+  // Fetch the config (clientId & secret)
+  var config = ServiceConfiguration.configurations.findOne({service: 'line'});
+  if (!config) {
+    credentialRequestCompleteCallback && credentialRequestCompleteCallback(new ServiceConfiguration.ConfigError());
+    return;
+  }
+
+  // Added on security used for the `state` param.
+  var credentialToken = Random.secret();
+  var loginStyle = OAuth._loginStyle('line', config, options);
+
+  // Generate the oauth URL.
+
+  var loginUrl =
+    'https://access.line.me/dialog/oauth/weblogin' +
+    '?response_type=code' +
+    '&client_id=' + config.clientId +
+    '&redirect_uri=' + OAuth._redirectUri('line', config) +
+    '&state=' + OAuth._stateParam(loginStyle, credentialToken);
+
+  OAuth.launchLogin({
+    loginService: "line",
+    loginStyle: loginStyle,
+    loginUrl: loginUrl,
+    credentialRequestCompleteCallback: credentialRequestCompleteCallback,
+    credentialToken: credentialToken
+  });
+};
