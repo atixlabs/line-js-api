65a22d50a67ae1f055286ab4523a8de14588b83b
Agustin Rosas <arosas@atixlabs.com>
2017-03-28 14:46:28 -0300

    Add validateMessage method
    This method allows to determine if a message received in the webhook
    url is valid or not, according to LINE API specs.

--- line-accounts/line-messaging-api.js
+++ line-accounts/line-messaging-api.js
@@ -1,3 +1,5 @@
+var crypto = require('crypto');
+
 LineApi = {};
 
 LineApi.registerService = function( channelSecret, channelAccessToken ) {
@@ -5,17 +7,23 @@ LineApi.registerService = function( channelSecret, channelAccessToken ) {
 }
 
 LineApi.sendMessage = function(userId, messages) {
-    // Header must contain Channel Access Token
-    var headers = {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${this.LineApi.channelAccessToken}`
-    };
-    // Body must contain the receipient userId and an array of messages
-    var data = {
-      "to": userId,
-      "messages": messages
-    };
-    HTTP.post('https://api.line.me/v2/bot/message/push', { headers, data });
-  }
+  // Header must contain Channel Access Token
+  var headers = {
+    "Content-Type": "application/json",
+    "Authorization": `Bearer ${this.LineApi.channelAccessToken}`
+  };
+  // Body must contain the receipient userId and an array of messages
+  var data = {
+    "to": userId,
+    "messages": messages
+  };
+  HTTP.post('https://api.line.me/v2/bot/message/push', { headers, data });
+};
+
+LineApi.validateMessage = function(body, signature) {
+  var hmac = crypto.createHmac('sha256', this.LineApi.channelSecret);
+  hmac.update(JSON.stringify(body));
+  return hmac.digest('base64') === signature;
+};
 
 export default LineApi;
